8fdf5f26adf456e6acb979ba83599aea5adf6a92
ShalmaliPrediqt <shalmali.chakraborty@prediqt.it>
2025-04-11 13:26:38 +0530

    Update career_service.py

--- portals/services/career_service.py
+++ portals/services/career_service.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from urllib.parse import quote
 from models.career import EducationLevel
 
-# Ensure the upload directory exists
+
 UPLOAD_FOLDER = Path("uploads")
 UPLOAD_FOLDER.mkdir(parents=True, exist_ok=True)
 
@@ -28,7 +28,7 @@ class JobApplicationService:
         if resume.content_type not in allowed_types:
             raise ValueError("Invalid file type. Only PDF, DOC, and DOCX are allowed.")
 
-        # Rename file using email address
+      
         file_extension = Path(resume.filename).suffix
         new_filename = f"{email_address}{file_extension}"
         file_location = UPLOAD_FOLDER / new_filename
@@ -40,14 +40,12 @@ class JobApplicationService:
         return self.generate_resume_url(new_filename)
 
     def create_job_application(self, application_data: JobApplicationSchema, resume_url: str):
-        """Save job application details to the database"""
-    # Convert Pydantic schema to a dictionary
-        application_data_dict = application_data.dict()
+       application_data_dict = application_data.dict()
     
-    # Add resume_url to the dictionary
+
         application_data_dict['resume_url'] = resume_url
     
-    # Now, use the dictionary to create the JobApplication object
+    
         job_application = JobApplication(**application_data_dict)
         self.db.add(job_application)
         self.db.commit()
@@ -56,7 +54,6 @@ class JobApplicationService:
 
 
     def get_all_applications(self):
-        """Fetch all job applications from the database"""
         return self.db.query(JobApplication).all()
 
 
@@ -65,5 +62,4 @@ class EducationService:
         self.db = db
 
     def get_all_education_levels(self):
-        """Fetch all education levels from the database"""
         return self.db.query(EducationLevel).all()
