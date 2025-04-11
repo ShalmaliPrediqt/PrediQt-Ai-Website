2636f679629028e2bb2af4334ff388f88aa6e8a6
ShalmaliPrediqt <shalmali.chakraborty@prediqt.it>
2025-04-11 13:25:28 +0530

    Update contact.py

--- portals/schemas/contact.py
+++ portals/schemas/contact.py
@@ -2,7 +2,7 @@ from typing import ClassVar
 from pydantic import BaseModel, EmailStr, validator
 from enum import Enum
 
-# Enum to restrict the values of interest
+
 class InterestEnum(str, Enum):
     client = "Client"
     partner = "Partner"
@@ -15,11 +15,11 @@ class ContactFormCreate(BaseModel):
     message: str
     find_us: str
     
-    # Word limit constants
+ 
     MAX_MESSAGE_WORDS: ClassVar[int] = 500
     MAX_FIND_US_WORDS: ClassVar[int] = 50
 
-    # Validator to check the word count of the message field (500 words limit)
+   
     @validator('message')
     def check_message_word_limit(cls, v):
         word_count = len(v.split())
@@ -27,7 +27,7 @@ class ContactFormCreate(BaseModel):
             raise ValueError(f"Message should not exceed {cls.MAX_MESSAGE_WORDS} words. Currently {word_count} words.")
         return v
 
-    # Validator to check the word count of the find_us field (50 words limit)
+    
     @validator('find_us')
     def check_find_us_word_limit(cls, v):
         word_count = len(v.split())
