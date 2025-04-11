3e54e550aeb0c94fc61ea9fce7d64aa30486434c
ShalmaliPrediqt <shalmali.chakraborty@prediqt.it>
2025-04-11 13:23:12 +0530

    Update contact.py

--- portals/models/contact.py
+++ portals/models/contact.py
@@ -8,6 +8,6 @@ class ContactForm(Base):
     id = Column(Integer, primary_key=True, index=True)
     full_name = Column(String, index=True)
     email_address = Column(String, unique=True, index=True)
-    interest = Column(String)  # Store as string in DB
+    interest = Column(String) 
     message = Column(String)
     find_us = Column(String)
