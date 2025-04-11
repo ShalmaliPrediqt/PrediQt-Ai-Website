180976c49507ddc20da9692ac29273875d9820a2
ShalmaliPrediqt <shalmali.chakraborty@prediqt.it>
2025-04-11 16:05:42 +0530

    database.py

--- database.py
+++ database.py
@@ -4,22 +4,14 @@ from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 
-# Load environment variables
 load_dotenv()
 
-# Database URL from .env file or default fallback
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:Shalmali@localhost/PrediQt_Ai")
 
-# Create database engine
 engine = create_engine(DATABASE_URL)
-
-# Create session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base class for models
 Base = declarative_base()
 
-# Dependency to get a database session
 def get_db():
     db = SessionLocal()
     try:
